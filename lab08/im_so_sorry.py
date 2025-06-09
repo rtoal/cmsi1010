fd6087953ee61fe22270e77e35284abe93c6b6df
Ray Toal <rtoal@lmu.edu>
2025-06-09 12:57:44 -0700

    Start lab 8

---
+++ lab08/im_so_sorry.py
@@ -0,0 +1,10 @@
+def blocks(n):
+    return 0 if n <= 0 else blocks(n - 1) + n
+
+
+# Here are sample calls to the blocks function just for fun:
+print(blocks(8))
+print(blocks(0))
+print(blocks(-1))
+print(blocks(1))
+print(blocks(995))
