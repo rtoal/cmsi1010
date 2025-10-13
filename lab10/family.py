f5b807bc3f9845823953a5904e33a5db611bdaee
Ray Toal <rtoal@lmu.edu>
2025-10-13 15:26:32 -0700

    Start lab 10

---
+++ lab10/family.py
@@ -0,0 +1,58 @@
+class Person:
+    def __init__(self, name, born=None, died=None, mom=None, dad=None):
+        self.name = name
+        self.born = born
+        self.died = died
+        self.mom = mom
+        self.dad = dad
+
+    def __str__(self):
+        span = f"({self.born or '?'}-{self.died or '?'})"
+        mom = f"{self.mom.name if self.mom is not None else '?'}"
+        dad = f"{self.dad.name if self.dad is not None else '?'}"
+        return f"{self.name} {span} mom: {mom}, dad: {dad}"
+
+
+odi = Person("Odelie Copele", born="1824", died="1873")
+paul = Person("Paul Grambois", born="1806")
+eugenie = Person("Eugénie Granbois", born="1838",
+                 died="1907", mom=odi, dad=paul)
+celeste = Person("Céleste Lamelle", born="1814", died="1877")
+joey_b = Person("Joey Baquié", born="1811", died="1882")
+ferdinand = Person("Ferdinand Baquié", born="1837",
+                   died="1883", mom=celeste, dad=joey_b)
+louise = Person("Louise Baquié", born="1868",
+                died="1945", mom=eugenie, dad=ferdinand)
+julie = Person("Julienne Montreuil", born="1796", died="1833")
+agenor = Person("Agenor Ramos", born="1803", died="1846")
+marie = Person("Marie Ramos", born="1826", died="1904", mom=julie, dad=agenor)
+marge = Person("Marguerite Cadeneth", born="1804", died="1870")
+giacamo = Person("Giacamo Martino", born="1806", died="1852")
+jacques = Person("Jacques Martinez", born="1822",
+                 died="1891", mom=marge, dad=giacamo)
+joseph = Person("Joseph Martinez", born="1864",
+                died="1926", mom=marie, dad=jacques)
+mildred = Person("Mildred Martinez", born="1911",
+                 died="1990", mom=louise, dad=joseph)
+jeanne_c = Person("Jeanne Chauvin", born="1840", died="1866")
+romain = Person("Romain Prévost", born="1832", died="1879")
+jeanne_p = Person("Jeanne Prévost", born="1864", mom=jeanne_c, dad=romain)
+louise_a = Person("Louise Aubin", born="1814", died="1909")
+pierre = Person("Pierre Fontaine", born="1818", died="1886")
+ernie = Person("Ernest Fontaine", born="1857",
+               died="1919", mom=louise_a, dad=pierre)
+suzanne = Person("Suzanne Fontaine", born="1894",
+                 died="1979", mom=jeanne_p, dad=ernie)
+vittoria = Person("Vittoria Trusiano", born="1813", died="1880")
+francesco = Person("Francesco Alioto", born="c1813", died="c1880")
+maria = Person("Maria Alioto", born="1834",
+               died=">1908", mom=vittoria, dad=francesco)
+concetta = Person("Concetta Buccafusca", born="c1795", died="1843")
+giuseppe = Person("Giuseppe Riggitano", born="c1786", died="1864")
+santo = Person("Santo Riggitano", born="1824",
+               died="c1898", mom=concetta, dad=giuseppe)
+salvatore = Person("Salvatore Riggitano", born="1876",
+                   died="1960", mom=maria, dad=santo)
+louis = Person("Louis Prevost", born="1920",
+               died="1997", mom=suzanne, dad=salvatore)
+leo = Person("Robert Prevost", born="1955", mom=mildred, dad=louis)
